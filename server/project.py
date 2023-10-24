911a08ccb41300b52c6bfd030191d2819954bf96
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2023-10-24 13:36:37 +0200

    Safeguard for nonsensical splits

--- server/project.py
+++ server/project.py
@@ -135,8 +135,8 @@ class Project:
                 start = current_split.start
                 end = current_split.end
 
-                # Overarching safeguard against 0-duration splits
-                if start == end:
+                # Overarching safeguard against 0-duration and nonsensical splits
+                if start >= end:
                     current_split = current_split.next
                     continue
                 clip_file_path = os.path.join(tmp, f"{str(idx)}.mp4")
